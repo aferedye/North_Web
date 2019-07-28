a57168b24dd92f56342b54acda20af0e9ed62586
Nevrawze <aferedye@gmail.com>
2019-07-28 14:50:27 +0200

    Ajout de la route principale pour le controller Devis

--- src/Controller/DevisController.php
+++ src/Controller/DevisController.php
@@ -5,10 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class DevisController
+ * @package App\Controller
+ * @Route("/devis")
+ */
 class DevisController extends AbstractController
 {
     /**
-     * @Route("/devis", name="devis")
+     * @Route("/", name="devis")
      */
     public function index()
     {
