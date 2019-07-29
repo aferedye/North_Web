db4bca889ccca6201ea77edc8176ffa3e336c478
Melanie Lecocq <melanielec@hotmail.fr>
2019-07-29 17:04:47 +0200

    Ajout contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,4 +17,6 @@ class ContactController extends AbstractController
             'controller_name' => 'ContactController',
         ]);
     }
+
+
 }
---
+++ src/Entity/contact.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin stagiaire
+ * Date: 29/07/2019
+ * Time: 14:25
+ */
