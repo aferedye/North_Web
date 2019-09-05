ed2758b2e89a1e1a63653c4dfcbb50b2cd1558e1
Feredye <aferedye@gmail.com>
2019-09-05 15:50:14 +0200

    Add function for Admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Devis;
+use App\Repository\DevisRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
@@ -17,11 +20,121 @@ class AdminController extends AbstractController
      */
     public function index()
     {
-
-
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
             'user' => $this->getUser()
         ]);
+    }
+
+    /**
+     * @Route("/admin/devis", name="admin_devis")
+     * @Security("is_granted('ROLE_ADMIN')")
+     */
+    public function devis()
+    {
+        $message = null;
+
+        $em = $this->getDoctrine()->getRepository(Devis::class);
+        $results = $em->findAll();
+
+        return $this->render('admin/devis.html.twig', [
+            'user' => $this->getUser(),
+            'results' => $results,
+            'message' => $message
+        ]);
+    }
+
+    /**
+     * @Route("/admin/devis/detail", name="admin_detail_devis")
+     * @Security("is_granted('ROLE_ADMIN')")
+     */
+    public function devisdetail() {
+
+        return $this->render('admin/devisdetail.html.twig', [
+            'user' => $this->getUser(),
+        ]);
+    }
+
+
+    /**
+     * @Route("/admin/devis/search/name", name="admin_devis_searchname")
+     * @Security("is_granted('ROLE_ADMIN')")
+     */
+    public function searchName(Request $request)
+    {
+        $message = null;
+        $results = null;
+
+        $names = $request->get("Nom");
+
+        /** @var DevisRepository $em */
+        $em = $this->getDoctrine()->getRepository(Devis::class);
+
+        if ($names != null) {
+            $results = $em->searchName($names);
+        } elseif($results == null) {
+            $message = 'Aucun nom ne correspond dans la BDD';
+        }
+
+        return $this->render('admin/devis.html.twig', [
+            'results' => $results,
+            'user' => $this->getUser(),
+            'message' => $message
+        ]);
+
+    }
+
+    /**
+     * @Route("/admin/devis/search/firstname", name="admin_devis_searchfirstname")
+     * @Security("is_granted('ROLE_ADMIN')")
+     */
+    public function searchFirstName(Request $request)
+    {
+        $message = null;
+        $results = null;
+        $firstnames = $request->get("Prenom");
+
+        /** @var DevisRepository $em */
+        $em = $this->getDoctrine()->getRepository(Devis::class);
+
+        if ($firstnames != null) {
+            $results = $em->searchFirstname($firstnames);
+        } elseif($results == null) {
+            $message = 'Aucun prénom ne correspond dans la BDD';
+        }
+
+        return $this->render('admin/devis.html.twig', [
+            'results' => $results,
+            'user' => $this->getUser(),
+            'message' => $message
+        ]);
+
+    }
+
+    /**
+     * @Route("/admin/devis/search/email", name="admin_devis_searchemail")
+     * @Security("is_granted('ROLE_ADMIN')")
+     */
+    public function searchEmail(Request $request)
+    {
+        $message = null;
+        $results = null;
+
+        $email = $request->get("Email");
+
+        /** @var DevisRepository $em */
+        $em = $this->getDoctrine()->getRepository(Devis::class);
+
+        if ($email != null) {
+            $results = $em->searchEmail($email);
+        } elseif($results == null) {
+            $message = 'Aucun email ne correspond dans la BDD';
+        }
+
+        return $this->render('admin/devis.html.twig', [
+            'results' => $results,
+            'user' => $this->getUser(),
+            'message' => $message
+        ]);
+
     }
 }
